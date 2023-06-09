9a560bcc1b19c0dcdde99d3e5e2e997c21d95952
ErnestoVeg <ernesto_vegaa@hotmail.com>
2023-06-09 14:28:10 -0700

    tercer commit

--- server/src/db.js
+++ server/src/db.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
-const DriverModel = require("./models/Driver");
-const TeamsModel = require("./models/Teams");
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
@@ -29,15 +27,12 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-DriverModel(sequelize);
-TeamsModel(sequelize);
-
-const { Driver, Teams } = sequelize.models;
+const { Driver, Team } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Driver.belongsToMany(Teams,{ through: 'driver_teams'});
-Teams.belongsToMany(Driver,{ through: 'driver_teams'});
+Driver.belongsToMany(Team,{ through: 'driver_team'});
+Team.belongsToMany(Driver,{ through: 'driver_team'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- server/src/models/Teams.js
+++ server/src/models/Teams.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   
-  sequelize.define('Driver', {
+  sequelize.define('Team', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
